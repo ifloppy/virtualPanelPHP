14a40042bb63bb69358c1723dee69ec9e17a1ff3
软盘君 <68799904+ifloppy@users.noreply.github.com>
2022-01-04 19:35:56 +0800

    增加功能，并微调代码格式
    增加显示php信息、复制文件、移动文件，并对后半部分代码稍作调整（也就只是在每个块之间加了两个换行，让我能看清它）

--- virtualpanel.php
+++ virtualpanel.php
@@ -47,17 +47,21 @@ function printW($s){
 
 //real
 echo "Logs:<br>";
-if($_GET['function'] == null){
+if($_GET['function'] == null){//如果没有输入function选项，则显示命令列表
     echo "Functions:<br>
     file_list([target]):list the files<br>
     file_realPathGet():Get the real path of your script<br>
     fileZip_unzip(from, [to]):unzip a file<br>
     fileZip_zip(from:your file, to:your zip file, toZip:the file in your zip file):zip a file<br>
-    file_wget(url, file):download a file from an url
+    file_wget(url, file):download a file from an url<br>
+    file_unlink(target):Process a file with unlink function<br>
+    file_copy(old, new):Copy a file<br>
+    file_move(old, new):Move/Rename a file(with function 'rename')<br>
+    system_phpinfo():show php information with php function<br>
     <br>";
 }
 
-switch ($_GET['function']){
+switch ($_GET['function']){//识别选择的功能
     case "file_list":
         printW("File-tree:");
         if($_GET['target']==null){
@@ -66,9 +70,13 @@ switch ($_GET['function']){
             fileShow($_GET["target"]);
         }
         break;
+
+
     case "file_realPathGet":
         printW("Real path:".getcwd());
         break;
+
+
     case "fileZip_unzip":
         if($_GET['from'] == null){
             printW("Need parameter: from");
@@ -76,6 +84,8 @@ switch ($_GET['function']){
         }
         zipUnzip($_GET['from'], $_GET['to']);
         break;
+
+
     case "fileZip_zipAdd":
         if($_GET['from'] == null or $_GET['to'] == null or $_GET['toZip'] == null){
             printW("Lost parameter");
@@ -83,12 +93,43 @@ switch ($_GET['function']){
             die("缺失参数");
         }
         zipAdd($_GET['from'], $_GET['to'], $_GET['toZip']);
+        break;
+
+
     case "file_wget":
         if($_GET['url'] == null || $_GET['file'] == null){
             printW("Lost parameter");
             die("缺失参数");
         }
         file_put_contents($_GET['file'],file_get_contents($_GET['url']));
+        break;
+
+    case "file_unlink":
+        if($_GET['target'] == null){
+            die("缺失参数");
+        }
+        var_dump(unlink($_GET['target']));
+        break;
+
+    case "file_copy":
+        if($_GET['old'] == null || $_GET['new'] == null){
+            die("缺失参数");
+        }
+        var_dump(copy($_GET["old"], $_GET['new']));
+        break;
+
+
+    case "file_move":
+        if($_GET['old'] == null || $_GET['new'] == null){
+            die("缺失参数");
+        }
+        var_dump(rename($_GET['old']), $_GET['new']);
+        break;
+
+
+    case "system_phpinfo":
+        phpinfo();
+        break;
     default:
         echo "Did nothing";
         break;
